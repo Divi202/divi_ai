26d257e14cda23a921fec4278fc46e8292b1fbb1
Divi202 <divya@yahoo.com>
2025-07-11 15:13:37 +0530

    updated the navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,62 +1,44 @@
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-} from "@/components/ui/navigation-menu";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div className=" flex bg-black text-white w-full h-16 align-items-center justify-center">
-      <NavigationMenu>
-        <NavigationMenuList>
-          {/* Logo  */}
-          <div className="justify-self-start">
-            <NavigationMenuItem>
-              <NavigationMenuLink asChild>
-                <Link href="/">
-                  {" "}
-                  <Image
-                    src="/Logo.svg"
-                    alt="Divi AI logo"
-                    width={70}
-                    height={35}
-                  />
-                </Link>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-          </div>
-          {/* Navbar navigation menu  */}
-          <div className="flex justify-end">
-            {/* Feature bookmark  */}
-            <NavigationMenuItem>
-              <NavigationMenuLink asChild>
-                <Link href="#features">Features</Link>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-            {/* Benefits bookmark  */}
-            <NavigationMenuItem>
-              <NavigationMenuLink asChild>
-                <Link href="#benefits">Benefits</Link>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-            {/* Join button - navigate to CTA section  */}
-            <NavigationMenuItem>
-              <NavigationMenuLink asChild>
-                <Button
-                  variant="outline"
-                  className="bg-custom-white text-custom-black"
-                >
-                  <Link href="#CTA">Join</Link>
-                </Button>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-          </div>
-        </NavigationMenuList>
-      </NavigationMenu>
+    <div className="w-full h-16 flex flex-row items-center justify-end bg-custom-black text-custom-white px-10 ">
+      {/* Logo */}{" "}
+      <div className=" flex flex-1 justify-self-start">
+        <Link href="/">
+          {" "}
+          <Image
+            src="/Logo.svg"
+            width={70}
+            height={70}
+            alt="Divi AI logo"
+            className="text-custom-white hover:opacity-80"
+          />{" "}
+        </Link>
+      </div>
+      {/* navlinks  + join btn*/}
+      <div className="flex flex-row items-center gap-8 ">
+        <ul className="flex flex-row gap-6 font-body font-normal text-custom-white">
+          <li>
+            <Link href="#Features" className="hover:opacity-80">
+              Features
+            </Link>
+          </li>
+          <li>
+            <Link href="#Benefits" className="hover:opacity-80">
+              Benefits
+            </Link>
+          </li>
+        </ul>
+        <Button
+          variant="outline"
+          className="font-body font-semibold rounded-4xl px-6 drop-shadow-custom-white-md bg-custom-white text-custom-black hover:bg-custom-white hover:opacity-80 "
+        >
+          <Link href="#CTA">Join</Link>
+        </Button>
+      </div>
     </div>
   );
 };
