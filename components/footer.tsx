5e7765ccf6a5375f953f60a09df47bdc6b48c3e5
Divi202 <divya@yahoo.com>
2025-07-12 14:53:03 +0530

    implement features-list section

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,66 @@
+import Link from "next/link";
+
+import Image from "next/image";
+
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <div className="w-full h-16 flex flex-row items-center justify-end bg-custom-black text-custom-white px-10 ">
+      {/* copywrite text  */}
+      <p className="font-body flex-1 font-normal text-custom-white">
+        © 2025 DiviAI. All rights reserved.
+      </p>
+      {/* Logo */}{" "}
+      <div className=" flex px-90 justify-center">
+        <Image
+          src="/Logo.svg"
+          width={70}
+          height={70}
+          alt="Divi AI logo"
+          className="text-custom-white "
+        />{" "}
+      </div>
+      {/* important pages link*/}
+      <div className="flex flex-row items-center gap-8 ">
+        <ul className="flex flex-row gap-6 font-body font-normal text-custom-white">
+          <li>
+            <Link href="/privacy-policy" className="hover:opacity-80">
+              Privacy Policy
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact-us" className="hover:opacity-80">
+              Contact Us
+            </Link>
+          </li>
+        </ul>
+        {/* social media links */}
+        <div className="flex flex-row gap-4 items-center ">
+          <Link
+            href="#instagram link"
+            className=" flex flex-row  hover:opacity-80"
+          >
+            <Image
+              src="/instagram.svg"
+              alt="instagram logo"
+              width={25}
+              height={25}
+            />
+          </Link>
+          <Link
+            href="#facebook link"
+            className="bg-custom-black hover:opacity-80"
+          >
+            <Image
+              src="/facebook.svg"
+              alt="facebook logo"
+              width={25}
+              height={25}
+            />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- components/sections/features-list.tsx
+++ components/sections/features-list.tsx
@@ -1,5 +1,55 @@
+import { Clock, Shirt, Smartphone } from "lucide-react";
+
 const FeaturesList = () => {
-  return <div>features-list</div>;
+  return (
+    <div className="bg-custom-white text-custom-black px-10 py-20 flex flex-col itmes-center justify-center gap-20">
+      {" "}
+      {/* section-heading  */}
+      <div className=" flex flex-col items-center justify-center gap-4.5">
+        <h2 className="text-5xl w-3/5 font-heading font-bold text-center">
+          Transform Your Wardrobe with AI Styling
+        </h2>
+        <p className="w-3/5 font-body font-light text-xl text-center">
+          Discover how our AI technology can revolutionize your fashion choices.
+          Get personalized outfit recommendations tailored just for you.
+        </p>
+      </div>
+      {/* features-list  */}
+      <div className="flex flex-row gap-30 justify-center ">
+        {/* feature-1  */}
+        <div className="flex flex-col text-center gap-2 items-center w-1/6">
+          <Shirt />
+          <h3 className="font-heading text-2xl font-bold">
+            AI-Powered Outfit Styling at Your Fingertips
+          </h3>
+          <p className="font-body font-light">
+            Experience the future of fashion with our innovative AI.
+          </p>
+        </div>
+        {/* feature-2 */}
+        <div className="flex flex-col text-center gap-2 items-center  w-70 ">
+          <Clock />
+          <h3 className="font-heading text-2xl font-bold">
+            Get Real-Time Fashion Advice from Our AI Stylist Anytime, Anywhere
+          </h3>
+          <p className="font-body font-light">
+            Receive instant feedback on your outfit choices with our smart AI
+            tool.
+          </p>
+        </div>
+        {/* feature-3  */}
+        <div className="flex flex-col text-center gap-2 items-center  w-1/6 ">
+          <Smartphone />
+          <h3 className="font-heading text-2xl font-bold">
+            Personalized Recommendations Just for You
+          </h3>
+          <p className="font-body font-light">
+            Get outfits that match your unique style preferences.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FeaturesList;
