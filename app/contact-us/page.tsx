867961ea072cfb8d52a4254728b80487674e087a
Divi202 <divya@yahoo.com>
2025-07-16 18:19:51 +0530

    updated the contact us page

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -2,7 +2,6 @@
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 const ContactUs = () => {
   const email = "support@diviai.com"; // replace with your email
@@ -13,23 +12,20 @@ const ContactUs = () => {
         <Card className="w-[500px] h-[300px] p-4 bg-custom-black text-custom-white flex flex-col items-center justify-center gap-4">
           <CardHeader className="w-full text-center">
             <CardTitle className="text-3xl font-bold">Contact Us</CardTitle>
-            <p className="text-sm text-custom-white py-3">
+            <p className="text-sm text-custom-white py-3 ">
               If you have any questions, feedback, or concerns, feel free to
               reach out to us.
             </p>
           </CardHeader>
           <CardContent className="w-full text-center">
             <div className="flex flex-col items-center gap-2">
-              <div className="flex gap-4 items-center">
+              <div className="flex gap-2 items-center">
                 {" "}
                 <p className="text-sm ">Email us at:</p>
                 <Link href={`mailto:${email}`}>
-                  <Button
-                    variant="outline"
-                    className="bg-custom-white text-custom-black "
-                  >
+                  <p className=" text-sm text-custom-white font-bold  ">
                     {email}
-                  </Button>
+                  </p>
                 </Link>
               </div>
               <p className=" text-muted-foreground text-xs text-custom-white mt-2">
