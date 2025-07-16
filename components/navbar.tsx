8defac36f1f76ba4c5100ef09cedcc0c3defce9e
Divi202 <divya@yahoo.com>
2025-07-16 18:04:29 +0530

    implemented the funtionality of hiding the navbar links on other pages except home

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,47 +1,53 @@
+"use client";
+
 import Link from "next/link";
 import { Button } from "./ui/button";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
-  const currentUrl = typeof window !== "undefined" ? window.location.href : "";
-  console.log("Current URL:", currentUrl);
+  const pathname = usePathname();
+  const isHomePage = pathname === "/";
 
   return (
     <div className="relative w-full h-16 flex flex-row items-center justify-end bg-custom-black text-custom-white px-10 fixed">
-      {/* Logo */}{" "}
+      {/* Logo */}
       <div className=" flex flex-1 justify-self-start">
         <Link href="/">
-          {" "}
           <Image
             src="/Logo.svg"
             width={70}
             height={70}
             alt="Divi AI logo"
             className="text-custom-white hover:opacity-80"
-          />{" "}
+          />
         </Link>
       </div>
+
       {/* navlinks  + join btn*/}
-      <div className="flex flex-row items-center gap-8 ">
-        <ul className="flex flex-row gap-6 font-body font-normal text-custom-white">
-          <li>
-            <Link href="#features-list" className=" hover:opacity-80 ">
-              Features
-            </Link>
-          </li>
-          <li>
-            <Link href="#benefits" className="hover:opacity-80">
-              Benefits
-            </Link>
-          </li>
-        </ul>
-        <Button
-          variant="outline"
-          className="font-body font-semibold rounded-4xl px-6 drop-shadow-custom-white-md bg-custom-white text-custom-black hover:bg-custom-white hover:opacity-80 "
-        >
-          <Link href="#CTA">Join</Link>
-        </Button>
-      </div>
+      {isHomePage && (
+        <div className="flex flex-row items-center gap-8 ">
+          <ul className="flex flex-row gap-6 font-body font-normal text-custom-white">
+            <li>
+              <Link href="#features-list" className=" hover:opacity-80 ">
+                Features
+              </Link>
+            </li>
+            <li>
+              <Link href="#benefits" className="hover:opacity-80">
+                Benefits
+              </Link>
+            </li>
+          </ul>
+
+          <Button
+            variant="outline"
+            className="font-body font-semibold rounded-4xl px-6 drop-shadow-custom-white-md bg-custom-white text-custom-black hover:bg-custom-white hover:opacity-80 "
+          >
+            <Link href="#CTA">Join</Link>
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
