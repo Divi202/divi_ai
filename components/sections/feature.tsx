bf16ebe8cb8176b4daa8f015688fcb1d3a61b687
Divi202 <divya@yahoo.com>
2025-07-17 20:36:20 +0530

    made the feature section responsive

--- components/sections/feature.tsx
+++ components/sections/feature.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const FeatureSection = () => {
   return (
-    <div className="flex-row justify-center items-center max-w-screen px-10 py-20 bg-custom-black text-custom-white">
-      <div className="flex gap-16.5 justify center items-center">
+    <div className="flex-row justify-center items-center max-w-screen px-5 py-10 md:px-10 md:py-20 bg-custom-black text-custom-white ">
+      <div className="flex flex-col lg:flex-row gap-4 md:gap-8 lg:gap-auto justify center items-center">
         <div className="flex justify-center items-center ">
           <Image
             src="/feature.svg"
@@ -13,11 +13,11 @@ const FeatureSection = () => {
             className="rounded-4xl object-cover border-3 border-custom-white shadow-lg shadow-custom-white"
           ></Image>
         </div>
-        <div className=" flex flex-col items-center justify-center gap-4.5 w-4/5">
-          <h2 className="text-5xl w-3/5 font-heading font-bold text-center">
+        <div className=" flex flex-col items-center justify-center gap-2 md:gap-4 w-4/5">
+          <h2 className="text-xl md:text-3xl lg:text-5xl w-4/5 font-heading font-bold text-center">
             Personalized Outfits Tailored Just for You
           </h2>
-          <p className="w-3/5 font-body font-light text-xl text-center">
+          <p className="text-sm md:text-lg lg:text-xl md:w-4/5 font-body font-light text-center">
             Our AI technology analyzes your unique preferences and style choices
             to create outfits that resonate with your personality. Experience a
             new way to dress, where every outfit feels like it was made just for
